d4ef913c48b7905bd57c52210b2f81a3935a6824
Saransh <saransh.jain.161@gmail.com>
2018-10-27 23:32:12 +0530

    create gameway file

---
+++ gameway.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Nexdere</title>
+<head>
+<body>
+    <form method = "POST"  >
+    <<fieldset class="_80 mar0 fieldset">>
+    <legend align ="center">
+    Gametype selection:
+    </legend>
+    <p align="center">
+	OFFLINE:<input type = "radio" name = "game" value = "offline"><br>
+
+	</p>
+    <p align="center">
+    ONLINE:<input type = "radio" name = "game" value = "online"><br>
+	</p>
+
+
+    </fieldset>
+</body>    
+</html>
