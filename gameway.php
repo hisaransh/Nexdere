2d7d436c85cebe3913173cbaa67da1a9d1d9389f
Saransh <saransh.jain.161@gmail.com>
2018-10-28 00:47:18 +0530

    Corrected gameway.php

--- gameway.php
+++ gameway.php
@@ -1,23 +1,33 @@
+<?php
+    session_start();
+?>
+
+<?php if(isset($_SESSION["userid"])) :?>
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Nexdere</title>
+    <title>Choose game</title>
 <head>
 <body>
-    <form method = "POST"  >
-    <<fieldset class="_80 mar0 fieldset">>
-    <legend align ="center">
-    Gametype selection:
-    </legend>
+    <form method = "POST" name="gameplayform" action="selectgame.php"  >
+    
     <p align="center">
-	OFFLINE:<input type = "radio" name = "game" value = "offline"><br>
-
-	</p>
+	OFFLINE:<input type="radio" name="game" id="offline" value="offline" onclick="fun1()"><br>
+    </p>
+    
     <p align="center">
-    ONLINE:<input type = "radio" name = "game" value = "online"><br>
+    ONLINE: <input type="radio" name="game" value="online" ><br>
 	</p>
+    <input type="submit" name="submit" value="gamewaySubmit">
 
+    <div id="offlineinfo"> </div>
 
-    </fieldset>
+    
 </body>    
 </html>
+<?php else :
+    session_destroy();
+    header("location: signin.php");
+?>
+
+<?php endif; ?>
