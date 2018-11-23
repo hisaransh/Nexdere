0262f5d16201b8d1b62117b145c50bb631669964
HARSH TYAGI <41909498+tyagiharsh@users.noreply.github.com>
2018-11-23 15:09:02 +0530

    sinup visuals updated

--- signup.php
+++ signup.php
@@ -4,33 +4,22 @@
 	<title>NexDeer</title>
 	<meta name = "viewport" content="width=device-width, initial-scale=1, user-scalable=no"/>
 	<title></title>
-	<!-- <link rel="stylesheet" href="css/bootstrap.min.cs --><!-- s"> -->
+	<link rel="stylesheet" href="css/bootstrap.min.cs --><!-- s">
 	<link rel="stylesheet" href="css/style.css">
-	<!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-	 -->
-	 <script>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	
+
+	<script>
 		function validate()
 		{
 			var userName = document.forms["signupform"]["name"].value; 
 			var phoneno = document.forms["signupform"]["phone"].value;
 			var password1 = document.forms["signupform"]["password"].value;
 			var password2 = document.forms["signupform"]["password2"].value;
-			var error = "";
-			var length = password1.length;
-            if(length<8)
-            {
-                error = "Length of password shoud be greater then 8";
-				document.getElementById('forerror').innerHTML = error;
-				return false;
-            }
-			if(password1 != password2)
-			{
-				error = "Password and confirm password are not same";
-				document.getElementById('forerror').innerHTML = error;
-				return false;
-			}
-			
-			length = userName.length;
+			var error = ""; 
+            
+			var length = userName.length;
             //Name field should not be empty
             if(userName == ""){                      
                 error = "Name feild is Necessary";
@@ -45,17 +34,37 @@
                 }
                 else if(userName[i] >=0 && userName[i] <=9)
                 {
-                    error = "Name feild can't contain numbers";
+                    error = "<strong>Name feild can't contain numbers</strong>";
 					document.getElementById('forerror').innerHTML = error;
+					document.getElementById('forerror').classList.remove("none");
                     return false;
                 }
             }
 
+            // validation of password
+            length = password1.length;
+            if(length<8)
+            {
+                error = "<strong>Length of password shoud be greater then 8</strong>";
+				document.getElementById('forerror').innerHTML = error;
+				document.getElementById('forerror').classList.remove("none");
+				return false;
+            }
+			if(password1 != password2)
+			{
+				error = "<strong>Password and confirm password are not same</strong>";
+				document.getElementById('forerror').innerHTML = error;
+				document.getElementById('forerror').classList.remove("none");
+				return false;
+			}
+
+			//validation of phone
 			length = phoneno.length;
 			if(length!=10)
 			{
-				error = "Enter valid phone no of 10 digits";
+				error = "<strong> Enter valid phone no of 10 digits </strong>";
 				document.getElementById('forerror').innerHTML = error;
+				document.getElementById('forerror').classList.remove("none");
 				return false;
 			}
 			for(var i = 0;i<length;i++)
@@ -64,7 +73,9 @@
                     continue;
                 }
                 else{
-                    error = "Phone Number should be in numbers";
+                    error = "<strong>Phone Number should be in numbers</strong>";
+                    document.getElementById('forerror').innerHTML = error;
+					document.getElementById('forerror').classList.remove("none");
                     return false;
                 }
             }			
@@ -72,52 +83,96 @@
 		}
 	 </script>
 </head>
+
 <body class="body bg-image">
-	<form  method="POST" action = "registeringOnDatabase.php"  name="signupform" onsubmit="return validate()"  class=" _80 mar0">
-		<fieldset class="_80 mar0 fieldset">
-			<legend align ="center" class="white ">
-				<i><b>Registration Form :</b></i>
-			</legend>
-			<p align="center" class="white">
-				<label for="email" class="label" >Email:</label><br>
-				<input type="email" name="email" class="_40 input" required>
-			</p>
 
-			<p align="center" class="white">
-				<label for="password">Password:</label><br>
-				<input type="password" name="password" class="_40 input" required>
-			</p>
-
-			<p align="center" class="white">
-				<label for="confirmpassword">Confirm Password:</label><br>
-				<input type="password" name="password2" class="_40 input" required>
-			</p>
-
-			<p align="center" class="white">
-				<label for="name">Name:</label><br>
-				<input type="text" name="name" class="_40 input" required>
-			</p>
-			<p align="center" class="white">
-				<label for="gender">Gender:</label><br><br>
-				Male: <input type="radio" name="gender" value = "male">
-				Female:<input type="radio" name="gender" value = "female">
-			</p>
-			<p align="center" class="white">
-				<label for="dob">DateOfBirth:</label><br>
-				<input type="date" name="dob" class="_40 input" max=<?php
+	<div class="card">
+	  <h5 class="card-header center">Welcome to the Nexder World</h5>
+	  <div class="card-body">
+	  	<div class="right">
+	  		<span><b><i>Already registered..?</i></b></span><br>
+	  		<a href="signin.php " class="btn btn-primary right">Sign in -></a>
+	  	</div>
+	    <h5 class="card-title center">Sign-Up</h5>
+	    	<form  method="POST" action = "registeringOnDatabase.php"  name="signupform" onsubmit="return validate()"  class="mar0">
+				<fieldset class="_40 center2  ">
+					<legend align ="center" class=" ">
+						<i><b>Registration Form :</b></i>
+					</legend>
+					<div class="input-group mb-3 center ">
+					 	<div class="input-group-prepend">
+					    	<span class="input-group-text" id="inputGroup-sizing-default"> Email: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+					    	</span>
+					  	</div>
+					  	<input type="email" name="email" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default"
+					  	required>
+					</div>
+					<div class="input-group mb-3 center ">
+					 	<div class="input-group-prepend">
+					    	<span class="input-group-text" id="inputGroup-sizing-default"> Name: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+					    	</span>
+					  	</div>
+					  	<input type="text" name="name" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" required>
+					</div>
+					<div class="input-group mb-3 center ">
+					 	<div class="input-group-prepend">
+					    	<span class="input-group-text" id="inputGroup-sizing-default"> Password:&nbsp;&nbsp;&nbsp;
+					    	</span>
+					  	</div>
+					  	<input type="Password" name="password" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" required>
+					</div>
+					<div class="input-group mb-3 center ">
+					 	<div class="input-group-prepend">
+					    	<span class="input-group-text" id="inputGroup-sizing-default"> Re-Password:
+					    	</span>
+					  	</div>
+					  	<input type="Password" name="password2" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" required>
+					</div>
+					<div class="input-group mb-3 center ">
+					 	<div class="input-group-prepend">
+					    	<span class="input-group-text" id="inputGroup-sizing-default"> Phone No:
+					    	</span>
+					  	</div>
+					  	<input type="Phone" name="phone" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" required>
+					</div>
+					<div class="form-check form-check-inline ">
+		  				<input class="form-check-input" type="radio" name="gender" id="inlineRadio1" value="male">
+		 			 	<label class="form-check-label" for="inlineRadio1">Male</label>
+					</div>
+					<div class="form-check form-check-inline">
+		  				<input class="form-check-input" type="radio" name="gender" id="inlineRadio2" value="female">
+		  				<label class="form-check-label" for="inlineRadio2">Female</label>
+					</div>
+					<br>
+					<br>
+					<div class="input-group mb-3 center ">
+					 	<div class="input-group-prepend">
+					    	<span class="input-group-text" id="inputGroup-sizing-default"> DOB: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+					    	</span>
+					  	</div>
+					  	<input type="date" name="dob" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" max=<?php
                         echo date('Y-m-d');
-                            ?> required>
-			</p>
+                            ?> required> 
+					</div>
+					<br>
+					<div class="alert alert-warning alert-dismissible fade show none" role="alert" id="forerror">
+						<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+						<span aria-hidden="true">&times;</span>
+						</button>
+					</div>
 
-			<p align="center" class="white">
-				<label for="phone">Phone No:</label><br>
-				<input type="tel" name="phone" class="_40 input" required>
-			</p>
-			<p align="center" class="">
-				<input type="submit" name="submit" value = "SignUp" class=" _30 input submit" required>
-			</p>
-		</fieldset>
-	</form>
-		<div id='forerror'> </div>
+					<div class="" >
+						<button type="submit" class="btn btn-primary" name="submit" value = "SignUp" required>Sign up</button>
+					</div>
+				</fieldset>	
+			</form>
+	  </div>
+	</div>
+
 </body>
+
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
 </html>
