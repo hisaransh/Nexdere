b121de69c29e69b97131afc38492654e0928e018
stardoxx <ashutoshsinghyadav03@gmail.com>
2018-11-23 11:44:02 +0530

    add transition buttons

--- signin.php
+++ signin.php
@@ -35,6 +35,7 @@
 			</p>
 		</fieldset>
 	</form>
+	<button type="button" onclick="location.href='signup.php">signup</button>
 </body>
 </html>
 <?php
--- signup.php
+++ signup.php
@@ -9,46 +9,45 @@
 	<!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	 -->
 </head>
-<body class="body bg-image">
+<body>
 	<form  method="POST" action = "registeringOnDatabase.php"  class=" _80 mar0">
 		<fieldset class="_80 mar0 fieldset">
-			<legend align ="center" class="white ">
-				<i><b>Registration Form :</b></i>
+			<legend align ="center">
+				Registration Form :
 			</legend>
-			<p align="center" class="white">
-				<label for="email" class="label" >Email:</label><br>
+			<p align="center" class="">
+				<label for="email" >Email:</label><br>
 				<input type="email" name="email" class="_40 input" required>
 			</p>
 
-			<p align="center" class="white">
+			<p align="center" class="">
 				<label for="password">Password:</label><br>
 				<input type="password" name="password" class="_40 input" required>
 			</p>
 
-			<p align="center" class="white">
+			<p align="center" class="">
 				<label for="name">Name:</label><br>
 				<input type="text" name="name" class="_40 input" required>
 			</p>
-			<p align="center" class="white">
-				<label for="gender">Gender:</label><br><br>
+			<p align="center" class="">
+				<label for="gender">Gender:</label><br>
 				Male: <input type="radio" name="gender" value = "male">
 				Female:<input type="radio" name="gender" value = "female">
 			</p>
-			<p align="center" class="white">
+			<p align="center" class="">
 				<label for="dob">DateOfBirth:</label><br>
-				<input type="date" name="dob" class="_40 input" max=<?php
-                        echo date('Y-m-d');
-                            ?> required>
+				<input type="date" name="dob" class="_40 input" required>
 			</p>
 
-			<p align="center" class="white">
+			<p align="center" class="">
 				<label for="phone">Phone No:</label><br>
 				<input type="tel" name="phone" class="_40 input" required>
 			</p>
 			<p align="center" class="">
-				<input type="submit" name="submit" value = "SignUp" class=" _30 input submit" required>
+				<input type="submit" name="submit" value = "submit" required>
 			</p>
 		</fieldset>
 	</form>
+	<button type="button" onclick="location.href='signin.php">signip</button>
 </body>
 </html>
