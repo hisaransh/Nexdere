ab09303bf2bbc555862c019edb374c4f88c22ba1
HARSH TYAGI <41909498+tyagiharsh@users.noreply.github.com>
2018-11-27 01:34:19 +0530

    Merge branch 'master' of https://github.com/tyagiharsh/Nexdere

--- index.php
+++ index.php
@@ -26,16 +26,20 @@
         <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
         <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
         <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
       </ol>
       <div class="carousel-inner">
         <div class="carousel-item active">
-          <img class="d-block w-100" src="img/test1.jpg" alt="First slide">
+          <img class="d-block w-100" src="img/football.jpg" width="660px" height="400px" alt="First slide">
         </div>
         <div class="carousel-item">
-          <img class="d-block w-100" src="img/test1.jpg" alt="Second slide">
+          <img class="d-block w-100" src="img/try.jpg" width="660px" height="400px" alt="Second slide">
         </div>
         <div class="carousel-item">
-          <img class="d-block w-100" src="img/test3.jpg" alt="Third slide">
+          <img class="d-block w-100" src="img/pubg.jpg" width="660px" height="400px" alt="Third slide">
+        </div>
+         <div class="carousel-item">
+          <img class="d-block w-100" src="img/cricket.jpg" width="660px" height="400px" alt="Fourth slide">
         </div>
       </div>
       <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
@@ -53,25 +57,37 @@
 
     <div class="center2 mar0">
       <div class="card left  " style="width: 18rem;">
-        <img class="card-img-top" src="img/title.png" alt="Card image cap">
+        <img class="card-img-top" src="img/title.png" width="200px" height="200px" alt="Card image cap">
         <div class="card-body">
-          <p class="card-text">Experience a new way of gaming and let the adrenaline rush into your veins.</p>
+          <p class="card-text"><strong>Experience a new way of gaming and let the adrenaline rush into your veins.</strong></p>
         </div>
       </div>
 
       <div class="card left " style="width: 18rem; left: 35px;">
-        <img class="card-img-top" src="img/title.png" alt="Card image cap">
+        <img class="card-img-top" src="img/csgoonline.jpeg" width="200px" height="210px" alt="Card image cap">
         <div class="card-body">
-          <p class="card-text">Play with comfort at your place as per your convenience.</p>
+          <p class="card-text"><strong>Play with comfort at your place as per your convenience.</strong></p>
+          <h2></h2>
         </div>
       </div>
       <div class="card " style="width: 18rem; left: 60px;">
-        <img class="card-img-top" src="img/title.png" alt="Card image cap">
+        <img class="card-img-top" src="img/fifa2.jpg" width="200px" height="200px" alt="Card image cap">
         <div class="card-body">
-          <p class="card-text">Just ground and a sport are all that you need to remain fit and enjoy.</p>
+          <p class="card-text"><strong>Just ground and a sport are all that you need to remain fit and enjoy.</strong></p>
         </div>
       </div>
       
+      </div>
+    </div>
+
+    <div class="card _70 center2">
+      <div class="card-header">
+        <center><strong> Get your tournament Registered</strong></center>
+      </div>
+      <div class="card-body">
+        <h5 class="card-title"></h5>
+        <p class="card-text">Let your tournament on Fire & Make others to fuel it</p>
+        <a href="addTournament.php" class="btn btn-primary">Register</a>
       </div>
     </div>
 
